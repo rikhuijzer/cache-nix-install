ae69c7fe2dd292beef2d4255652dcc4f8b42f8c1
Rik <rik@huijzer.xyz>
2020-09-03 21:11:39 +0200

    Change

--- src/restore.js
+++ src/restore.js
@@ -1,2 +1,9 @@
-const shell = require('shelljs')
-shell.exec('./restore.sh')
+const { exec } = require('child_process');
+var yourscript = exec('sh restore.sh',
+        (error, stdout, stderr) => {
+            console.log(stdout);
+            console.log(stderr);
+            if (error !== null) {
+                console.log(`exec error: ${error}`);
+            }
+        });
