61a5fe3562635ac88d68141cca7155edbc9e9931
Rik <rik@huijzer.xyz>
2020-09-03 21:52:07 +0200

    Change

--- src/core.sh
+++ src/core.sh
@@ -2,21 +2,26 @@
 
 set -e
 
-CALLER="$1"
+function install_nix {
+    export INPUT_NIX_PATH="nixpkgs=channel:${{ INPUT_NIX_VERSION }}"
+    export INPUT_SKIP_ADDING_NIXPKGS_CHANNEL="false"
 
-printenv
-
-function main {
     git clone --branch v10 https://github.com/cachix/install-nix-action /tmp/cachix
     nodejs /tmp/cachix/lib/main.js
+}
+
+function install_via_nix {
+    nix-env --install --file ${{ INPUT_NIX_FILE }}
 }
 
 function post {
     echo Came from post
 }
 
+CALLER="$1"
 if [ "$CALLER" == "main" ]; then
-    main
+    install_nix
+    install_via_nix
 elif [ "$CALLER" == "post" ]; then
     post
 else
