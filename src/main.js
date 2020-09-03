5aa03846eb9be30975e94d9a6a3a88c4a1aa0069
Rik <rik@huijzer.xyz>
2020-09-03 21:30:19 +0200

    Change

--- src/main.js
+++ src/main.js
@@ -1,15 +1,3 @@
+const utils = require('./utils.js')
 
-function run_script(script) {
-    const { exec } = require('child_process');
-    exec(script,
-        (error, stdout, stderr) => {
-            console.log(stdout);
-            console.log(stderr);
-            if (error !== null) {
-                console.log(`exec error: ${error}`);
-                process.exit(1);
-            }
-    });
-}
-
-run_script('./src/restore.sh')
+utils.run_script('./src/restore.sh')
---
+++ src/utils.js
@@ -0,0 +1,12 @@
+function run_script(script) {
+    const { exec } = require('child_process');
+    exec(script,
+        (error, stdout, stderr) => {
+            console.log(stdout);
+            console.log(stderr);
+            if (error !== null) {
+                console.log(`exec error: ${error}`);
+                process.exit(1);
+            }
+    });
+}
