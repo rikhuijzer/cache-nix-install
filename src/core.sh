69d168c647bc5e05ee469affca2f4014ea38c1af
Rik <rik@huijzer.xyz>
2020-09-04 10:07:15 +0200

    Simplify

--- src/core.sh
+++ src/core.sh
@@ -1,9 +1,33 @@
 #!/bin/bash
 
+set -e
+
+function install_dependencies {
+    sudo apt-get update
+    sudo apt-get install -y nodejs
+}
+
+function install_nix {
+    export INPUT_NIX_PATH="nixpkgs=channel:$INPUT_NIX_VERSION"
+    export INPUT_SKIP_ADDING_NIXPKGS_CHANNEL="false"
+
+    git clone --branch v10 https://github.com/cachix/install-nix-action /tmp/cachix
+    nodejs /tmp/cachix/lib/main.js
+}
+
 function install_via_nix {
     nix-env --install --file "$INPUT_NIX_FILE"
 }
 
-printenv
-
-install_via_nix
+CALLER="$1"
+if [ "$CALLER" == "main" ]; then
+    install_dependencies
+    install_nix
+    printenv
+    install_via_nix
+elif [ "$CALLER" == "post" ]; then
+    post
+else
+    echo "No argument given"
+    exit 1
+fi
--- src/main.js
+++ src/main.js
@@ -1,21 +1,2 @@
 const utils = require('./utils.js')
-const { spawn } = require('child_process');
-
-child = spawn('./src/prepare.sh');
-
-child.stdout.setEncoding('utf8');
-child.stdout.on('data', function(data) {
-    console.log('stdout: ' + data);
-});
-
-child.stderr.setEncoding('utf8');
-child.stderr.on('data', function(data) {
-    console.log('stderr: ' + data);
-});
-
-child.on('exit', function(exitCode) {
-    if (parseInt(exitCode) !== 0) {
-        // Handle non-zero exit.
-    }
-    utils.run_script('./src/core.sh main')
-});
+utils.run_script('./src/core.sh main')
