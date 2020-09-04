c8d1d9d00bf4a8d2b3b0b5231e4c541c1e2d0462
Rik <rik@huijzer.xyz>
2020-09-04 13:46:49 +0200

    Change

--- src/install_via_nix_or_cache.sh
+++ src/install_via_nix_or_cache.sh
@@ -8,16 +8,15 @@ NIX_BIN=/nix/var/nix/profiles/per-user/$USER/profile/bin
 NIX_STORE=/nix/store
 
 if [[ "$CACHE_HIT" != 'true' ]]; then
-  nix-env --install --file "$INPUTS_NIX_FILE"
-  mkdir ~/nix
-  which hello
-  ls -ahl $NIX_BIN
-  sudo mount -o remount,rw $NIX_STORE
-  sudo mv $NIX_STORE ~/nix/store
-  sudo mv $NIX_BIN ~/nix/bin
+    nix-env --install --file "$INPUTS_NIX_FILE"
+    mkdir ~/nix
+    which hello
+    ls -ahl $NIX_BIN
+    sudo mv $NIX_STORE ~/nix/store
+    sudo mv $NIX_BIN ~/nix/bin
 else
-  sudo mkdir --parents $NIX_STORE
-  sudo mkdir --parents $NIX_BIN
-  sudo mv --force ~/nix/store $NIX_STORE
-  sudo mv --force ~/nix/bin $NIX_BIN
+    sudo mkdir --parents $NIX_STORE
+    sudo mkdir --parents $NIX_BIN
+    sudo mv --force ~/nix/store $NIX_STORE
+    sudo mv --force ~/nix/bin $NIX_BIN
 fi
