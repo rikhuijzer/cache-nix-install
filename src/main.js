5500b04aa83ff2b8e21ab320c09f98bbf266fef0
Rik <rik@huijzer.xyz>
2020-09-04 10:31:05 +0200

    Change

--- src/main.js
+++ src/main.js
@@ -3,12 +3,12 @@ var child = spawn('./src/core.sh', ['main'])
 
 child.stdout.setEncoding('utf8');
 child.stdout.on('data', function(data) {
-    console.log('stdout: ' + data);
+    console.log(data);
 });
 
 child.stderr.setEncoding('utf8');
 child.stderr.on('data', function(data) {
-    console.log('stderr: ' + data);
+    console.log(data);
 });
 
 child.on('close', function(exitCode) {
