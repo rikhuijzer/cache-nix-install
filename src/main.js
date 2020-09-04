24f0fcfdb0683a99c36434a74f242a6eb6eddb32
Rik <rik@huijzer.xyz>
2020-09-04 09:39:47 +0200

    Use emitter

--- src/main.js
+++ src/main.js
@@ -1,2 +1,15 @@
-const utils = require('./utils.js')
-utils.run_script('./src/parent.sh main')
+const { spawn } = require('child_process');
+const events = require('events');
+const myEmitter = new events.EventEmitter();
+
+firstSpawn = spawn('./src/prepare.sh');
+firstSpawn.on('exit'), (exitCode) => {
+    if (parseInt(code) !== 0) {
+        //Handle non-zero exit
+    }
+    myEmitter.emit('firstSpawn-finished');
+}
+
+myEmitter.on('firstSpawn-finished', () => {
+    secondSpawn = spawn('./src/core.sh', ['main'])
+})
