e6bf3ea58fa52bfa97b10bf81b72fa77b01cad61
Rik <rik@huijzer.xyz>
2020-09-04 09:54:53 +0200

    Change

--- src/main.js
+++ src/main.js
@@ -1,12 +1,13 @@
+const utils = require('./utils.js')
 const { spawn } = require('child_process');
 
 firstSpawn = spawn('./src/prepare.sh');
 
-function on_exit(exitCode) {
+function spawn_second(exitCode) {
     if (parseInt(exitCode) !== 0) {
         // Handle non-zero exit.
     }
-    secondSpawn = spawn('./src/core.sh', ['main'])
+    utils.run_script('./src/core.sh main')
 }
 
-firstSpawn.on('exit', on_exit)
+firstSpawn.on('exit', spawn_second)
