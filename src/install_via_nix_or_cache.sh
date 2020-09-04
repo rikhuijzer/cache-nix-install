2f6e0b43b717b06be3680fa56da46c47ee41d02f
Rik <rik@huijzer.xyz>
2020-09-04 14:23:19 +0200

    Change

--- src/install_via_nix_or_cache.sh
+++ src/install_via_nix_or_cache.sh
@@ -4,7 +4,7 @@ echo ""
 echo "running install_via_nix_or_cache.sh"
 echo ""
 
-NIX_BIN=/nix/var/nix/profiles/per-user/$USER/profile/bin
+NIX_BIN=/nix/var/nix/profiles/per-user/runner/profile/bin
 NIX_STORE=/nix/store
 
 if [[ "$CACHE_HIT" != 'true' ]]; then
