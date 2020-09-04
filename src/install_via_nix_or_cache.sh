2277f1f2e0339a8c21166076610bafc3e9288ef2
Rik <rik@huijzer.xyz>
2020-09-04 15:53:51 +0200

    Change chown

--- src/install_via_nix_or_cache.sh
+++ src/install_via_nix_or_cache.sh
@@ -10,9 +10,7 @@ NIX_BIN=/nix/var/nix/profiles/per-user/$USER/profile/bin
 if [[ "$CACHE_HIT" != 'true' ]]; then
     nix-env --install --file $INPUTS_NIX_FILE
     echo "Relaxing permissions for /nix/store"
-    sudo chown --recursive $(id -u):$(id -g) /nix/store
-    echo "Relaxing permissions for /run/current-system/sw/bin"
-    sudo chown --recursive $(id -u):$(id -g) /run/current-system/sw/bin
+    sudo chown --recursive "$USER:" /nix/store
 else
     which hello
 fi
