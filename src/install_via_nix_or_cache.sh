07043cf434a4156a19f2b90f6b5e9dc28c991865
Rik <rik@huijzer.xyz>
2020-09-04 15:40:46 +0200

    Relaxing

--- src/install_via_nix_or_cache.sh
+++ src/install_via_nix_or_cache.sh
@@ -11,6 +11,8 @@ if [[ "$CACHE_HIT" != 'true' ]]; then
     nix-env --install --file $INPUTS_NIX_FILE
     echo "Relaxing permissions for /nix/store"
     sudo chown --recursive $(id -u):$(id -g) /nix/store
+    echo "Relaxing permissions for /run/current-system/sw/bin
+    sudo chown --recursive $(id -u):$(id -g) /run/current-system/sw/bin
 else
     which hello
 fi
