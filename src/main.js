54d87bdee7c1a2cf5faf34e324f2348e9c2e9fa6
Rik <rik@huijzer.xyz>
2020-09-04 09:53:09 +0200

    Change

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ const { spawn } = require('child_process');
 firstSpawn = spawn('./src/prepare.sh');
 
 function on_exit(exitCode) {
-    if (parseInt(code) !== 0) {
+    if (parseInt(exitCode) !== 0) {
         // Handle non-zero exit.
     }
     secondSpawn = spawn('./src/core.sh', ['main'])
