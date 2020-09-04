d390ece814e894b60b9d86130e6636104e2c5ba6
Rik <rik@huijzer.xyz>
2020-09-04 10:12:17 +0200

    Improve logging

--- src/main.js
+++ src/main.js
@@ -1,2 +1,16 @@
-const utils = require('./utils.js')
-utils.run_script('./src/core.sh main')
+var spawn = require('child_process').spawn
+var child = spawn('./src/core.sh', ['main'])
+
+child.stdout.setEncoding('utf8');
+child.stdout.on('data', function(data) {
+    console.log('stdout: ' + data);
+});
+
+child.stderr.setEncoding('utf8');
+child.stderr.on('data', function(data) {
+    console.log('stderr: ' + data);
+});
+
+child.on('close', function(exitCode) {
+    console.log('Process exited with: ' + exitCode);
+});
