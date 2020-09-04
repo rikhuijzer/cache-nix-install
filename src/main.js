a618049465b8bd9f6db392e5c473e22863ad1738
Rik <rik@huijzer.xyz>
2020-09-04 10:00:28 +0200

    Change

--- src/main.js
+++ src/main.js
@@ -1,13 +1,32 @@
 const utils = require('./utils.js')
 const { spawn } = require('child_process');
 
-firstSpawn = spawn('./src/prepare.sh');
+child = spawn('./src/prepare.sh');
 
-function spawn_second(exitCode) {
+function after(exitCode) {
     if (parseInt(exitCode) !== 0) {
         // Handle non-zero exit.
     }
     utils.run_script('./src/core.sh main')
 }
 
-firstSpawn.on('exit', spawn_second)
+child.stdout.setEncoding('utf8');
+child.stdout.on('data', function(data) {
+    console.log('stdout: ' + data);
+    data=data.toString();
+    scriptOutput+=data;
+});
+
+child.stderr.setEncoding('utf8');
+child.stderr.on('data', function(data) {
+    console.log('stderr: ' + data);
+    data=data.toString();
+    scriptOutput+=data;
+});
+
+firstSpawn.on('exit', function(code) {
+    if (parseInt(exitCode) !== 0) {
+        // Handle non-zero exit.
+    }
+    utils.run_script('./src/core.sh main')
+});
