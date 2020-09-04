493c2aac3208ba2d20e05344c3b33335e8868acb
Rik <rik@huijzer.xyz>
2020-09-04 15:43:38 +0200

    Fix typo

--- src/install_via_nix_or_cache.sh
+++ src/install_via_nix_or_cache.sh
@@ -11,7 +11,7 @@ if [[ "$CACHE_HIT" != 'true' ]]; then
     nix-env --install --file $INPUTS_NIX_FILE
     echo "Relaxing permissions for /nix/store"
     sudo chown --recursive $(id -u):$(id -g) /nix/store
-    echo "Relaxing permissions for /run/current-system/sw/bin
+    echo "Relaxing permissions for /run/current-system/sw/bin"
     sudo chown --recursive $(id -u):$(id -g) /run/current-system/sw/bin
 else
     which hello
