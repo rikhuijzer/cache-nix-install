c8985d1805e9144f50c3f80b819802ad31ed90e2
Rik <rik@huijzer.xyz>
2020-09-04 09:44:49 +0200

    Avoid emitter

--- src/main.js
+++ src/main.js
@@ -1,15 +1,10 @@
 const { spawn } = require('child_process');
 const events = require('events');
-const myEmitter = new events.EventEmitter();
 
 firstSpawn = spawn('./src/prepare.sh');
 firstSpawn.on('exit'), (exitCode) => {
     if (parseInt(code) !== 0) {
-        //Handle non-zero exit
+        // Handle non-zero exit.
     }
-    myEmitter.emit('firstSpawn-finished');
-}
-
-myEmitter.on('firstSpawn-finished', () => {
     secondSpawn = spawn('./src/core.sh', ['main'])
-})
+}
