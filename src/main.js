84383c2ec0096c68bbcc4dc355828ccc1928d56f
Rik <rik@huijzer.xyz>
2020-09-04 09:46:07 +0200

    Change syntax

--- src/main.js
+++ src/main.js
@@ -1,10 +1,9 @@
 const { spawn } = require('child_process');
-const events = require('events');
 
 firstSpawn = spawn('./src/prepare.sh');
-firstSpawn.on('exit'), (exitCode) => {
+firstSpawn.on('exit'), function(exitCode) {
     if (parseInt(code) !== 0) {
         // Handle non-zero exit.
     }
     secondSpawn = spawn('./src/core.sh', ['main'])
-}
+};
